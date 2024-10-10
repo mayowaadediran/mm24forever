67f951b1c478030c61a53eab87ce4678216ae5f8
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2024-10-10 15:35:07 +0100

    fix rsvp page

--- src/components/pages/rsvp-page/index.tsx
+++ src/components/pages/rsvp-page/index.tsx
@@ -10,22 +10,14 @@ const RSVPPage = () => {
         title='RSVP'
         description="We are really excited to see you, and hope you can join us for our special day without any hesitation"
       />
-      <div>
-        <div className='mb-20'>
+      <div className='flex justify-center'>
+        <div className='mb-20 text-center'>
           <h3 className='font-montaga text-lg font-medium lg:text-lg text-primary mb-6'>
             Please let us know if you plan to attend our special day.
           </h3>
           <p className='font-montaga text-base font-medium text-primary'>Toyin - 0802 318 0107</p>
           <p className='font-montaga text-base font-medium text-primary'>Fisayo - 0708 823 6524</p>
         </div>
-        {/* <div className=''>
-          <div className='inline-block min-w-[200px]'>
-            <label htmlFor="" className="font-montaga text-sm block">
-              Name
-            </label>
-            <input className="w-full border-primary/50 border rounded-sm	focus:border-primary outline-none h-[45px] pl-2 " />
-          </div>
-        </div> */}
       </div>
     </div>
   )
